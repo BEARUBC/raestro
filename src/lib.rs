e8852a1fa10e8afaf00f4e0366c556a59d9f21bc
nathan-n-poon <nathannpoon@gmail.com>
2021-01-30 16:49:56 -0700

    refactor main into a test

--- src/lib.rs
+++ src/lib.rs
@@ -10,23 +10,25 @@ use rppal::system::DeviceInfo;
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_UART_TX: u8 = 14;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
-
-    let uart = Uart::new(115_200, Parity::None, 8, 1)?;
-
-    // Blink the LED by setting the pin's logic level high for 500 ms.
-    uart.set_high();
-    thread::sleep(Duration::from_millis(500));
-    uart.set_low();
-
-    Ok(())
-}
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn lights() {
+        println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+        let uart = Uart::new(115_200, Parity::None, 8, 1)?;
+
+        // Blink the LED by setting the pin's logic level high for 500 ms.
+        uart.set_high();
+        thread::sleep(Duration::from_millis(500));
+        uart.set_low();
+
+        Ok(())
     }
 }
