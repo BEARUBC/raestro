70c339044b5e92cf5f7e5421f4a5aa05de7aaa90
Raunak Bhagat <rabhagat31@gmail.com>
2021-01-30 17:18:20 -0700

    working through uart implementation

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use rppal::uart::Parity;
+use rppal::uart::{
+    Parity,
+    Uart,
+};
 use rppal::system::DeviceInfo;
-
-
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_UART_TX: u8 = 14;
@@ -21,16 +22,19 @@ mod tests {
     }
 
     #[test]
-    fn lights() {
-        println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
-
-        let uart = Uart::new(115_200, Parity::None, 8, 1)?;
-
+    fn lights() -> () {
+        println!("Blinking an LED on a {}.", super::DeviceInfo::new().unwrap().model());
+        let uart = super::Uart::new(115_200, super::Parity::None, 8, 1);
+        
+        match uart {
+            Ok(_) => println!("uart init work"),
+            Err(_) => println!("uart init failed"),
+        };
         // Blink the LED by setting the pin's logic level high for 500 ms.
-        uart.set_high();
-        thread::sleep(Duration::from_millis(500));
-        uart.set_low();
+        // uart.set_high();
+        // thread::sleep(Duration::from_millis(500));
+        // uart.set_low();
 
-        Ok(())
+        // Ok(())
     }
 }
