e12379e372a1e89e5e4c53dda7f8bf4f2c328865
Raunak Bhagat <rabhagat31@gmail.com>
2021-01-31 00:34:43 -0700

    added panic to failure case

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,16 @@ mod tests {
 
     #[test]
     fn lights() -> () {
-        println!("Blinking an LED on a {}.", super::DeviceInfo::new().unwrap().model());
+        // println!("Blinking an LED on a {}.", super::DeviceInfo::new().unwrap().model());
         let uart = super::Uart::new(115_200, super::Parity::None, 8, 1);
         
         match uart {
             Ok(_) => println!("uart init work"),
-            Err(_) => println!("uart init failed"),
+            Err(err) =>{
+                println!("{:?}", err);
+                panic!();
+            },
+
         };
         // Blink the LED by setting the pin's logic level high for 500 ms.
         // uart.set_high();
