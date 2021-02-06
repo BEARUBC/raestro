0d48507ddba3e4749985b36a1fb2449633ed6b35
Raunak Bhagat <rabhagat31@gmail.com>
2021-02-06 16:29:53 -0700

    moved constants into new enum into new file

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_UART_TX: u8 = 14u8;
+mod gpio;
 
-const BAUD_RATE: u32 = 50u32;
-const DATA_BITS: u8 = 8u8;
-const STOP_BITS: u8 = 1u8;
-const BUFFER: &[u8; 1usize] = &[0x01u8];
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+// const GPIO_UART_TX: u8 = 14u8;
+// const GPIO_UART_RX: u8 = 15u8;
+// const BAUD_RATE: u32 = 50u32;
+// const DATA_BITS: u8 = 8u8;
+// const STOP_BITS: u8 = 1u8;
 
 #[cfg(test)]
 mod tests {
@@ -23,6 +24,10 @@ mod tests {
         },
         system::DeviceInfo,
     };
+    use super::gpio::{
+        UartMetaData,
+        BUFFER,
+    };
  
     #[test]
     fn it_works() {
@@ -32,15 +37,18 @@ mod tests {
     #[test]
     fn lights() -> () {
         // println!("Blinking an LED on a {}.", super::DeviceInfo::new().unwrap().model());
-        let mut uart: Uart = Uart::new(BAUD_RATE, Parity::None, DATA_BITS, STOP_BITS).unwrap();
-        // uart.set_baud_rate(115_200u32);
-        // uart.set_baud_rate(50u32);
-        // uart.set_data_bits(8u8);
-        // uart.set_stop_bits(1u8);
+        let mut uart: Uart = Uart::new(
+            UartMetaData::BAUDRATE as u32,
+            Parity::None,
+            UartMetaData::DATABITS as u8,
+            UartMetaData::STOPBITS as u8
+        )
+            .unwrap();
+
         uart.send_start();
 
         loop {
-            uart.write(BUFFER);
+            uart.write(&BUFFER);
         }
 
         // Blink the LED by setting the pin's logic level high for 500 ms.
