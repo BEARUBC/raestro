cd5680c14d68aa74a5b5d2651ddfea8d602617cb
Raunak Bhagat <rabhagat31@gmail.com>
2021-02-06 15:53:43 -0700

    uart is initialized with 8 data bits and 1 stop bit; moved magic numbers into global constants

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@ use std::thread;
 use std::time::Duration;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_UART_TX: u8 = 14;
+const GPIO_UART_TX: u8 = 14u8;
+
+const BAUD_RATE: u32 = 50u32;
+const DATA_BITS: u8 = 8u8;
+const STOP_BITS: u8 = 1u8;
+const BUFFER: &[u8; 1usize] = &[0x01u8];
 
 #[cfg(test)]
 mod tests {
@@ -27,15 +32,15 @@ mod tests {
     #[test]
     fn lights() -> () {
         // println!("Blinking an LED on a {}.", super::DeviceInfo::new().unwrap().model());
-        let mut uart: Uart = Uart::new(115_200, Parity::None, 8u8, 1u8).unwrap();
+        let mut uart: Uart = Uart::new(BAUD_RATE, Parity::None, DATA_BITS, STOP_BITS).unwrap();
         // uart.set_baud_rate(115_200u32);
-        uart.set_baud_rate(50u32);
+        // uart.set_baud_rate(50u32);
+        // uart.set_data_bits(8u8);
+        // uart.set_stop_bits(1u8);
         uart.send_start();
 
-        let buffer: &[u8; 1usize] = &[0x01u8];
-
         loop {
-            uart.write(buffer);
+            uart.write(BUFFER);
         }
 
         // Blink the LED by setting the pin's logic level high for 500 ms.
