2a4995fb661415506ee9ac0d5dbb44e740b1e93a
Raunak Bhagat <rabhagat31@gmail.com>
2021-04-11 01:03:27 -0600

    tried testing why the servos are not extending fully... unsuccessful in finding out why; edited microsec_to_target to not multiply data by 4

--- src/bin/swivel_servo.rs
+++ src/bin/swivel_servo.rs
@@ -13,15 +13,56 @@ fn main() -> () {
     let mut maestro: Maestro = Maestro::new();
     maestro.start(BaudRates::BR_115200).unwrap();
 
-    let small: u16 = 500u16;
-    let big: u16 = 60000u16;
-    let sleep_time: u64 = 1000u64;
+    // 0x0090u16 seems to be the minimum with 4x multiplier
+    // 0x0200u16 seems to be the maximum with 4x multiplier
+
+    // let p0: u16 = 1500u16;
+    // let p1: u16 = 6000u16;
+    // let p2: u16 = 8000u16;
+    // let p3: u16 = 6000u16;
+    // let sleep_time: u64 = 1000u64;
+
+    // loop {
+    //     println!("{}", p0);
+    //     maestro.set_target(Channels::C_0, p0).unwrap();
+    //     thread::sleep(Duration::from_millis(sleep_time));
+    //     println!();
+
+    //     println!("{}", p1);
+    //     maestro.set_target(Channels::C_0, p1).unwrap();
+    //     thread::sleep(Duration::from_millis(sleep_time));
+    //     println!();
+
+    //     println!("{}", p2);
+    //     maestro.set_target(Channels::C_0, p2).unwrap();
+    //     thread::sleep(Duration::from_millis(sleep_time));
+    //     println!();
+
+    //     println!("{}", p3);
+    //     maestro.set_target(Channels::C_0, p3).unwrap();
+    //     thread::sleep(Duration::from_millis(sleep_time));
+    //     println!();
+    // }
+
+    let mut position = 5_000u16;
+    let a = 5_000u16;
+    let b = 8_000u16;
+    let sleep_time: u64 = 200u64;
 
     loop {
-        maestro.set_target(Channels::C_0, small).unwrap();
+        println!("{}", position);
+        maestro.set_target(Channels::C_0, position).unwrap();
         thread::sleep(Duration::from_millis(sleep_time));
 
-        maestro.set_target(Channels::C_0, big).unwrap();
-        thread::sleep(Duration::from_millis(sleep_time));
+        // position += 100u16;
+        // if position == 8_000u16 {
+        //     position = 5_000u16;
+        // }
+
+        if position == a {
+            position = b;
+        } else if position == b {
+            position = a;
+        }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -10,14 +10,11 @@ pub(crate) fn mask_byte(byte: u8) -> u8 {
     return byte & top_mask;
 }
 
-pub(crate) fn microsec_to_target(mut microsec: u16) -> (u8, u8) {
-    let multiplier: u8 = 2u8;
-    let down_shift: u8 = 7u8;
+pub(crate) fn microsec_to_target(microsec: u16) -> (u8, u8) {
+    let down_shift = 7usize;
 
-    microsec <<= multiplier;
-
-    let lower: u8 = mask_byte(microsec as u8);
-    let upper: u8 = mask_byte((microsec >> down_shift) as u8);
+    let lower = mask_byte(microsec as u8);
+    let upper = mask_byte((microsec >> down_shift) as u8);
 
     return (lower, upper);
 }
@@ -57,9 +54,9 @@ mod tests {
 
     #[test]
     fn simple_short_to_target_test() -> () {
-        let short: u16 = 1500u16;
+        let target: u16 = 6000u16;
         let expected: (u8, u8) = (0x70u8, 0x2eu8);
 
-        assert_eq!(microsec_to_target(short), expected);
+        assert_eq!(microsec_to_target(target), expected);
     }
 }
