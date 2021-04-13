dd78571441ef6e08ec90e11692df5ee9365afeba
Raunak Bhagat <rabhagat31@gmail.com>
2021-04-13 15:38:11 -0600

    edit get_positions to iterator over an array of positions

--- src/bin/get_position.rs
+++ src/bin/get_position.rs
@@ -13,23 +13,22 @@ fn main() -> () {
     let mut maestro: Maestro = Maestro::new();
     maestro.start(BaudRates::BR_115200).unwrap();
 
-    let small: u16 = 500u16;
-    let big: u16 = 60000u16;
+    // this is the maximum range, apparently...?
+    //      (i.e., 3968 - 8000)
+    //      (which is equal to 992us - 2000us)
+    let positions: [u16; 2usize] = [3968u16, 8000u16];
     let sleep_time: u64 = 1000u64;
 
+    #[allow(unused_assignments)]
+    let mut arr: Option<u16> = None;
+
     loop {
-        maestro.set_target(Channels::C_0, small).unwrap();
-        let arr = maestro.get_position(Channels::C_0).unwrap();
-
-        println!("position: {:?}", arr);
-
-        thread::sleep(Duration::from_millis(sleep_time));
-
-        maestro.set_target(Channels::C_0, big).unwrap();
-        let arr = maestro.get_position(Channels::C_0).unwrap();
-
-        println!("position: {:?}", arr);
-
-        thread::sleep(Duration::from_millis(sleep_time));
+        for (index, position) in positions.iter().enumerate() {
+	        maestro.set_target(Channels::C_0, *position).unwrap();
+	        arr = Some(maestro.get_position(Channels::C_0).unwrap());
+	
+	        println!("position_{}: {:?}", index, arr.unwrap());
+	        thread::sleep(Duration::from_millis(sleep_time));
+        }
     }
 }
