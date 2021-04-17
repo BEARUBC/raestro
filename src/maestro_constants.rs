33cf69854201b1415c894f12039daba3a25d815e
Raunak Bhagat <rabhagat31@gmail.com>
2021-04-17 05:25:00 -0600

    add legal copyright message to top of each code file

--- src/maestro_constants.rs
+++ src/maestro_constants.rs
@@ -1,3 +1,10 @@
+// Copyright 2021 UBC Bionics, Ltd.
+//
+// Licensed under the MIT license
+// <LICENSE.md or https://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
 /* external uses */
 
 /* internal mods */
