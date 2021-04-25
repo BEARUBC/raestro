aff9d243299cc8c0dd5f3f3e72e98b421798c925
Raunak Bhagat <rabhagat31@gmail.com>
2021-04-25 09:49:32 -0600

    more edits on docs

--- src/maestro.rs
+++ src/maestro.rs
@@ -51,18 +51,18 @@ pub struct Maestro {
 /// This section contains all the APIs required to get a `maestro` instance up and running.
 ///
 /// ## Definitions
-/// An 'initialized' `maestro` instance is defined as an instance in which struct fields are the `Some(_)` variant.
+/// An `initialized  `maestro` instance is defined as an instance in which struct fields are the `Some(_)` variant.
 ///
-/// An 'uninitialized' `maestro` instance is an instance in which all fields are the `None` variant.
+/// An `uninitialized` `maestro` instance is an instance in which all fields are the `None` variant.
 ///
-/// An 'invalid' `maestro` instance is an instance which does not fit the above definitions.
+/// An `invalid` `maestro` instance is an instance which does not fit the above definitions.
 ///
-/// All valid `maestro` instances must either uninitialized or initialized. An invalid `maestro` instance will result in undefined behaviour.
-/// All methods below are proved to either only transition a `maestro` instance to uninitialized and initialized states only.
-/// Therefore, invalid states are impossible to reach.
+/// All valid `maestro` instances must either `uninitialized` or `initialized`. An `invalid` `maestro` instance will result in undefined behaviour.
+/// All methods below are proved to either only transition a `maestro` instance to `uninitialized` and `initialized` states only.
+/// Therefore, `invalid` states are impossible to reach.
 /// This is an invariant which must (and will) be guaranteed for all provided APIs.
 impl Maestro {
-    /// Creates a new 
+    /// Creates a new `maestro` instance in the `uninitialized` state.
     pub fn new() -> Self {
         return Maestro {
             uart: None,
@@ -71,13 +71,13 @@ impl Maestro {
         };
     }
 
-    /// Transitions the given `maestro` instance to the 'initialized state' with default configuration values.
+    /// Transitions the given `maestro` instance to the `initialized` state with default configuration values.
     ///
     /// Default block duration is set to `2seconds`.
     ///
     /// Returns an error if any fields were unable to be initialized.
-    /// In the case of this failure, this instance will be closed and will be transitioned *back* into the 'uninitialized' state.
-    /// This is done to prevent any leakage of `maestro` instances into the 'invalid' state.
+    /// In the case of this failure, this instance will be closed and will be transitioned *back* into the `uninitialized` state.
+    /// This is done to prevent any leakage of `maestro` instances into the `invalid` state.
     pub fn start(self: &mut Self, baud_rate: BaudRates) -> Result<()> {
         let uart_result: RppalResult<Uart> = Uart::new(
             baud_rate as u32,
@@ -110,7 +110,7 @@ impl Maestro {
             .map_err(|rppal_err| Error::from(rppal_err));
     }
 
-    /// Closes the `maestro` instance (i.e., transitions the `maestro` instance *back* into the 'uninitialized' state).
+    /// Closes the `maestro` instance (i.e., transitions the `maestro` instance *back* into the `uninitialized` state).
     ///
     /// This instance is no longer usable to communicate with the Maestro, unless until `Maestro::start()` is called again.
     pub fn close(self: &mut Self) -> () {
